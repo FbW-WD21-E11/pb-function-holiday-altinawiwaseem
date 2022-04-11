73f236249683bbf1a2565a13bc2ef5bd622ac662
altinawiwaseem <wasemm@live.com>
2022-04-12 01:05:27 +0200

    editing the first task

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 // test function
-function isEven(number){
-    if (number % 2 === 0){
-        return `is this number: ${number} even? true`
-    } else {
-        return `is this number: ${number} even? false`
-    }
-    
-}
-let result = isEven(4)
-console.log(result)
+let isEven = (number) => {
+  return `is this number even?: ${number % 2 === 0} `;
+};
+let result = isEven(4);
+console.log(result);
 
 // task 1
 
-function isInRange(number, min, max){
-     if (number >= min && number <=max){
-        return `is this number ${number} in range between ${min} and ${max} ? true`
-    } else {
-        return `is this number: ${number} in range between ${min} and ${max} ? false`
-    } 
-      
+let isInRange = (number, min, max) => {
+  if (number >= min && number <= max) {
+    return `is this number ${number} in range between ${min} and ${max} ? true`;
+  } else {
+    return `is this number: ${number} in range between ${min} and ${max} ? false`;
+  }
+};
+console.log(isInRange(4, 2, 4));
+
+// task 2
+let vowels = "a, e, i, o, u";
+function containsVowel(str) {
+  let array = []
+  for (let i = 0; i < str.length; i++) {
+    array.push(str[i])
+    
+  };
+  
+  /* return vowels.includes(string) ? true: false */
+  return vowels.includes(array)
 }
-console.log (isInRange(4,2,4)) 
+console.log(containsVowel("egfhesd")) ;
+
+
+// task 3
