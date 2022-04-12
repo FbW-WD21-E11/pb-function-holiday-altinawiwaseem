554d4b06039eee53d4f204928c53618e05063628
altinawiwaseem <wasemm@live.com>
2022-04-12 18:37:07 +0200

    editing the first task

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 // test function
-let isEven = (number) => {
-  return `is this number even?: ${number % 2 === 0} `;
-};
-let result = isEven(4);
+let isEven = number => `is this number even?: ${number % 2 === 0} `;
+
+let result = isEven(2);
 console.log(result);
 
 // task 1
@@ -17,18 +16,43 @@ let isInRange = (number, min, max) => {
 console.log(isInRange(4, 2, 4));
 
 // task 2
-let vowels = "a, e, i, o, u";
-function containsVowel(str) {
-  let array = []
-  for (let i = 0; i < str.length; i++) {
-    array.push(str[i])
-    
-  };
+
+
+  let vowels = "a, e, i, o, u";
+  let str = "table"
+ // for (let i = 0; i < str.length; i++){
+    if (str.toLowerCase().includes(vowels)){
+      console.log(true)
+    }else {
+      console.log(false)
+    }
   
-  /* return vowels.includes(string) ? true: false */
-  return vowels.includes(array)
-}
-console.log(containsVowel("egfhesd")) ;
+  
+  
+  
 
 
 // task 3
+let numbers;
+let containsNumber = string =>{
+  
+  for (let i = 0; i < string.length; i++){
+    
+      if(typeof parseInt(string[i]) === "number")
+       numbers += parseInt(string[i])
+       console.log(string[i])
+       return numbers
+    
+  }
+  
+   
+}
+
+console.log(containsNumber("123")); // true
+//console.log(containsNumber("dg")); // false
+// containsNumber("a2c"); // true
+// containsNumber("100px"); // true
+// containsNumber("1"); // true
+// containsNumber("0"); // true
+// containsNumber("$23.00"); // true
+// containsNumber("T-shirts x2"); // true
