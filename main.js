72d4821ed6bcbc99c32a03ee089304992c6c1054
altinawiwaseem <wasemm@live.com>
2022-04-16 01:08:48 +0200

    5th task

--- main.js
+++ main.js
@@ -1,45 +1,40 @@
 // test function
-let isEven = number => { 
+let isEven = (number) => {
   return number % 2 === 0;
-}
+};
 
 console.log(isEven(4));
 
 // task 1
 
 let isInRange = (number, min, max) => {
-  
-    return number >= min && number <= max
-  
+  return number >= min && number <= max;
 };
 console.log(isInRange(3, 2, 4));
 
 // task 2
 
-function containsVowel (string){
+function containsVowel(string) {
   let vowels = "aeiou";
-  for (let i = 0; i < string.length; i++){
-   if( vowels.includes(string[i].toLowerCase())){
-      return true
-   }
-   }
-   return false
+  for (let i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i].toLowerCase())) {
+      return true;
+    }
   }
-  console.log(containsVowel("table")); // true
-  console.log(containsVowel("bcdfg")); // false
-  console.log(containsVowel("a")); // true
-  console.log(containsVowel("aeiou")); // true
-  console.log(containsVowel("AEIOU")); // true
-  console.log(containsVowel("")); // false
-  console.log(containsVowel("Abc")); // true
-  console.log(containsVowel("z,z")) //false
-  console.log(containsVowel("z z z")) // false
-  
-  
-
+  return false;
+}
+console.log(containsVowel("table")); // true
+console.log(containsVowel("bcdfg")); // false
+console.log(containsVowel("a")); // true
+console.log(containsVowel("aeiou")); // true
+console.log(containsVowel("AEIOU")); // true
+console.log(containsVowel("")); // false
+console.log(containsVowel("Abc")); // true
+console.log(containsVowel("z,z")); //false
+console.log(containsVowel("z z z")); // false
 
 // task 3
- /* console.log("\n task number 3")
+/* console.log("\n task number 3")
  function containsNumber(number){
 
  
@@ -50,49 +45,57 @@ function containsVowel (string){
    }
      return false
   } */
-  
-  console.log("\n task number 3")
-  function containsNumber(number){
- 
-  
-   for (let i = 0; i < number.length; i++){
-         if(!isNaN(parseFloat(number[i]))){   
-         return true
-     }
-    }
-      return false
-   }
-  
-    
-  
-  
 
-console.log(containsNumber("123")); // true 
+console.log("\n task number 3");
+function containsNumber(number) {
+  for (let i = 0; i < number.length; i++) {
+    if (!isNaN(parseFloat(number[i]))) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(containsNumber("123")); // true
 console.log(containsNumber("dg")); // false
 console.log(containsNumber("a2c")); // true
-console.log(containsNumber("100px")) ; // true
-console.log(containsNumber("1")) ; // true
-console.log(containsNumber("0")) ; // true
-console.log(containsNumber("$23.00")) ; // true
-console.log( containsNumber("T-shirts x2")); // true 
+console.log(containsNumber("100px")); // true
+console.log(containsNumber("1")); // true
+console.log(containsNumber("0")); // true
+console.log(containsNumber("$23.00")); // true
+console.log(containsNumber("T-shirts x2")); // true
 
-console.log("\n task 4")
+console.log("\n task 4");
 
-function isFirstLetterUpperCase (word){  
-  if (typeof word !== 'string' || word.length === 0) {
+function isFirstLetterUpperCase(word) {
+  if (typeof word !== "string" || word.length === 0) {
+    return false;
+  } else if (word[0] === word[0].toUpperCase()) {
+    return true;
+  } else {
     return false;
   }
-  else if ( word[0] === word[0].toUpperCase()){
-    return true
-  
-} else {
-  return false
-}
 }
 
-console.log(isFirstLetterUpperCase("hello"))
-console.log(isFirstLetterUpperCase("Hello"))
-console.log(isFirstLetterUpperCase(""))
-console.log(isFirstLetterUpperCase("A"))
-console.log(isFirstLetterUpperCase("Bianca"))
-console.log(isFirstLetterUpperCase("jim"))
+console.log(isFirstLetterUpperCase("hello"));
+console.log(isFirstLetterUpperCase("Hello"));
+console.log(isFirstLetterUpperCase(""));
+console.log(isFirstLetterUpperCase("A"));
+console.log(isFirstLetterUpperCase("Bianca"));
+console.log(isFirstLetterUpperCase("jim"));
+
+console.log("\n task 5");
+
+function cheaperThan(prices, number) {
+  let array = [];
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < number) {
+      array.push(prices[i]);
+    }
+  }
+  return array;
+}
+console.log(cheaperThan([19.99, 29.87, 12.99, 5.0, 2.9, 100], 20));
+console.log(cheaperThan([19.99, 29.87, 12.99, 5.00, 2.90, 100], 90))
+console.log(cheaperThan([19.99, 29.87, 12.99, 5.00, 2.90, 100], 0))
+console.log(cheaperThan([19.99, 29.87, 12.99, 5.00, 2.90, 100], 5))
